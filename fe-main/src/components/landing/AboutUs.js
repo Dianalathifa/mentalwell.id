4064c79a2148122ad567169faa54258fa17c1154
unknown <dl.lathif1803@student.uns.ac.id>
2024-04-06 16:34:21 +0700

    update UI Navbar

--- fe-main/src/components/landing/AboutUs.js
+++ fe-main/src/components/landing/AboutUs.js
@@ -15,9 +15,9 @@ const AboutUs = () => {
       <Col md={16} className="d-flex align-items-center justify-content-center">
         <div className="container text-left">
           <h6 className="section-title mb-2 tfonts">About Us</h6>
-          <h6 className="subtitle" style={{ fontSize: "28px" }}>Selamat datang di MentalWell<br></br>ruang digital yang didedikasikan untuk kesehatan mental.<br></br>
-          Kami adalah komunitas yang berfokus memberikan dukungan holistik dan sumber daya<br></br>untuk meningkatkan kesejahteraan mental Anda.<br></br>
-          MentalWell percaya bahwa setiap individu layak mendapatkan akses mudah dan aman<br></br>terhadap layanan kesehatan mental yang berkualitas.</h6> 
+          <h6 className="subtitle" style={{ fontSize: "28px" }}>Selamat datang di MentalWell ruang digital yang didedikasikan <br></br>untuk kesehatan mental.
+          Kami adalah komunitas yang berfokus memberikan dukungan holistik dan sumber daya untuk meningkatkan kesejahteraan mental anda. MentalWell percaya bahwa setiap individu layak mendapatkan akses mudah dan aman
+          terhadap layanan kesehatan mental yang berkualitas.</h6> 
         </div>
         <img src={logo1} alt="Logo" style={{ width: "500px", height: "560px", maxWidth: "100%", maxHeight: "100%" }} /> {/* Panggil gambar dengan variabel */}
       </Col>
@@ -35,12 +35,10 @@ const AboutUs = () => {
             <img src={image6} alt="Image" style={{ maxWidth: "100%", maxHeight: "100%" }} /> {/* Panggil gambar dengan variabel */}
           </Col>
           <Col md={8}>
-            <Card className="about-us-card">
-              <Card.Body>
+           
               <h5  style={{ fontSize: "30px", fontWeight:"bold" }}><br></br>VISI<br></br></h5>
                 <p style={{ fontSize: "25px" }}><br></br>Meningkatkan kesadaran tentang kesehatan mental <br></br>dan proses pengembangan diri untuk mewujudkan kualitas hidup yang lebih optimal<br></br><br></br></p>
-              </Card.Body>
-            </Card>
+             
           </Col>        
         </Row>
       </Container>
@@ -51,20 +49,18 @@ const AboutUs = () => {
             <img src={image7} alt="Image" style={{ maxWidth: "100%", maxHeight: "100%" }} /> {/* Panggil gambar dengan variabel */}
           </Col>
           <Col md={8}>
-            <Card className="about-us-card">
-              <Card.Body>
+            
               <h5  style={{ fontSize: "30px", fontWeight:"bold" }}><br></br>MISI<br></br></h5>
                 <p style={{ fontSize: "25px" }}><br></br>1. Memahami permasalahan kesehatan mental yang sedang dialami remaja khususnya mahasiswabr<br></br>
                                                           2. Mengedukasi remaja khususnya mahasiswa tentang kesehatan mental dan pengembangan diri melalui berbagai konten kreatif di platform website<br></br>
                                                           3. Menyediakan bantuan berupa layanan dukungan kesehatan mental dan pengembangan diri kepada remaja dan mahasiswa yang membutuhkan<br></br>
                                                           4. Memberikan pemahaman kepada kalangan remaja agar lebih mengenal diri sendiri</p>
-              </Card.Body>
-            </Card>
+             
           </Col>
         </Row>
       </Container>
 
-      
+      <br></br>
       <Container className="my-6">
         <Row className="justify-content-center">
           <Col md={9}>
@@ -75,9 +71,12 @@ const AboutUs = () => {
               </Card.Body>
             </Card>
           </Col>
-          
+
         </Row>
       </Container>
+      <br></br>
+      <br></br>
+
       <Footer />
     </div>
   );
--- fe-main/src/components/landing/Navbar.js
+++ fe-main/src/components/landing/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
+import { Navbar, Nav, Button } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom"; 
 import "../style/Home.css";
 import logo from "../images/logo.png";
@@ -9,41 +9,35 @@ const Header = () => {
   const currentPath = useLocation().pathname;
 
   return (
-    <Navbar expand="lg" style={{ backgroundColor: "#C4EAF4", position: "relative" }} fixed="top" shadow="2">
-      <Navbar.Brand href="#home">
+    <Navbar expand="lg" style={{ backgroundColor: "#C4EAF4", position: "relative", boxShadow: "0 2px 4px rgba(0,0,0,0.1)" }} fixed="top">
+      <Navbar.Brand href="/">
         <img
           src={logo}
           alt="MentalWellLogo"
-          width="10%" // Sesuaikan dengan tinggi yang diinginkan
+          width="13%" // Sesuaikan dengan tinggi yang diinginkan
           className="d-inline-block align-top"
         />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/" className={currentPath === "/" ? "active" : ""}>
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="/dailyinsight-user" className={currentPath === "/reserv-user" ? "active" : ""}>
-            Daily Insight
-          </Nav.Link>
-          {/* <NavDropdown
-            title="Mental Well Test"
-            id="basic-nav-dropdown"
-            className={currentPath === "/mentalwelltest-user" ? "nav-item active" : "nav-item"}
-          >
-            <NavDropdown.Item as={Link} to="/srqtest-user">SRQ-20 Test</NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/intervensi-user">Intervensi</NavDropdown.Item>
-          </NavDropdown> */}
-          <Nav.Link as={Link} to="/mentalwelltest-user" className={currentPath === "/mentalwelltest-user" ? "active" : ""}>
-            Mental Well Test
-          </Nav.Link><Nav.Link as={Link} to="/about-us" className={currentPath === "/about-us" ? "active" : ""}>
-            About Us
-          </Nav.Link>
-          <Nav.Link as={Link} to="/psikolog-list" className={currentPath === "/psikolog-list" ? "active" : ""}>
-            Psikolog List
-          </Nav.Link>
-        </Nav>
+      <Nav className="mr-auto">
+      <Nav.Link as={Link} to="/" className={`nav-link ${currentPath === "/" ? "active" : ""}`} style={{ color: "#000", fontWeight: "bold" }}>
+        Home
+      </Nav.Link>
+      <Nav.Link as={Link} to="/dailyinsight-user" className={`nav-link ${currentPath === "/reserv-user" ? "active" : ""}`} style={{ color: "#000", fontWeight: "bold" }}>
+        Daily Insight
+      </Nav.Link>
+      <Nav.Link as={Link} to="/mentalwelltest-user" className={`nav-link ${currentPath === "/mentalwelltest-user" ? "active" : ""}`} style={{ color: "#000", fontWeight: "bold" }}>
+        Mental Well Test
+      </Nav.Link>
+      <Nav.Link as={Link} to="/about-us" className={`nav-link ${currentPath === "/about-us" ? "active" : ""}`} style={{ color: "#000", fontWeight: "bold" }}>
+        About Us
+      </Nav.Link>
+      <Nav.Link as={Link} to="/psikolog-list" className={`nav-link ${currentPath === "/psikolog-list" ? "active" : ""}`} style={{ color: "#000", fontWeight: "bold" }}>
+        Psikolog List
+      </Nav.Link>
+    </Nav>
+
         <Nav>
           <Button variant="light" as={Link} to="/partisipan-login" type="button" className="btn btn-outline-info" style={{ backgroundColor: "#FFFFF", borderColor: "#239BB2", borderWidth: "3px" }}>
             Login
